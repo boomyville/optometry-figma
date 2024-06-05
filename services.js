c4e489fb1a223eb5c037a6ea2f275abb1816a92b
boomyville <boomyville@gmail.com>
2024-06-06 01:39:03 +1000

    Add files via upload

---
+++ services.js
@@ -0,0 +1,23 @@
+
+function shuffle(array) {
+    let currentIndex = array.length;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+
+        // Pick a remaining element...
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+    }
+
+
+  let rand = [0,1,2,3,4,5];
+  shuffle(rand);
+  document.querySelectorAll(".details2")[rand[0]].style.display = "none";
+  document.querySelectorAll(".details2")[rand[1]].style.display = "none";
+  document.querySelectorAll(".details2")[rand[2]].style.display = "none";
