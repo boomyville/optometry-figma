992a9a2f7cbf0dbde4f8760152de6b5690eb9586
boomyville <boomyville@gmail.com>
2024-06-06 12:51:52 +1000

    Add files via upload

--- auto.js
+++ auto.js
@@ -5,3 +5,12 @@ $(function(){
   $(function(){
     $(".bottom").load("bottom.html"); 
   });
+
+  function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
